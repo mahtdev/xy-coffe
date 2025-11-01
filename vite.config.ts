76371f48e5072c6dfab532cf2f3deff4d9594f41
Miguel Hernandez <mhernandez@kssoluciones.ocm>
2025-11-01 07:36:56 +0000

    Fix: Configurar base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/xy-coffe/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
